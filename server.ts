23eeb0c5812bd6846ebaf4e2be2987f04da84093
Manu254rce <straussmoabit@gmail.com>
2023-11-20 19:28:25 +0300

    chore: configured Port number as an env_var

--- server.ts
+++ server.ts
@@ -1,1 +1,19 @@
-import express from 'express';
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use((req: { method: any; path: any; }, res: any, next: () => void) => {
+    console.log('Request logged:', req.method, req.path);
+    next();
+});
+
+app.get('/', (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
+    res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
