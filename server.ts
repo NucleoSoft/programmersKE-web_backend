e216932c424a60dcd0242f23389aaea7156193bf
Manu254rce <straussmoabit@gmail.com>
2023-11-21 11:10:54 +0300

    chore: connected to MongoDB server

--- server.ts
+++ server.ts
@@ -1,19 +1,29 @@
 require('dotenv').config();
 
-const express = require('express');
+import express from 'express';
+const mongoose = require('mongoose');
+const languagesRoutes = require('./routes/languages');
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
 app.use((req: { method: any; path: any; }, res: any, next: () => void) => {
     console.log('Request logged:', req.method, req.path);
     next();
 });
 
-app.get('/', (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
-    res.json({ message: 'Hello World!' });
+app.use('/api/languages',languagesRoutes);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+}).then(() => {
+    app.listen(port, () => {
+        console.log(`Connected to MongoDB: Server started at http://localhost:${port}`);
+    });
+}).catch((err: any) => {
+    console.log(err);
 });
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+
